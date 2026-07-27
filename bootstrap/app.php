3e38f454fcbe81264d861193d841364d8bcc80f0
Renan Bernordi <altendorfme@gmail.com>
2026-07-27 12:32:00 -0300

    add trusted proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*');
+        $middleware->trustProxies(at: (string) env('TRUSTED_PROXIES', '172.16.0.0/12,10.0.0.0/8,192.168.0.0/16,127.0.0.1'));
 
         $middleware->web(append: [
             \App\Http\Middleware\SecurityHeaders::class,
